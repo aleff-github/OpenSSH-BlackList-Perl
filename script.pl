ae4f27ab061350bfa241b537bcd597f30cf5e069
Alessandro <alessandrogreco@hacari.org>
2021-06-10 11:07:50 +0200

    Add files via upload

--- script.pl
+++ script.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 # Indicare il path del file da voler analizzare tramite una sintassi ben precisa come argomento
 # perl script.pl p=<file_path>
 die "Assicurati di aver seguito correttamente la sintassi richiesta\n\nperl script.pl p=<file_path>\n" if($#ARGV < 0 or $#ARGV > 0);
@@ -28,12 +27,41 @@ for(@attacchi){
     push(@tutti_gli_ip,"$1\n");
 }
 
+# Elimino i duplicati
 %tutti = ();
 for (@tutti_gli_ip) {
     $tutti{$_}++;
 }
 @ip = keys %tutti;
 
+# Salvo gli ip in un file blacklist
 open(BLACKLIST,">","blacklist") or die $!;
 print BLACKLIST @ip;
+close BLACKLIST;
+
+@analisi;
+for(@ip){
+    $tmp_ip = $_;
+    @whois = qx{whois $tmp_ip};
+    push(@analisi, "#####################################\n");
+    push(@analisi, "# IP Rilevato: $tmp_ip");
+    for(@whois){
+        if($_ =~ m/(inetnum)/ or $_ =~ m/(netname)/){
+            push(@analisi, "[NET] $_");
+        }
+        elsif($_ =~ m/(descr)/ or $_ =~ m/(organisation)/){
+            push(@analisi, "[DESC] $_");
+        }
+        elsif($_ =~ m/(address)/ or $_ =~ m/(country)/){
+            push(@analisi, "[GEO] $_");
+        }
+        elsif($_ =~ m/(mail)/ or $_ =~ m/(phone)/ or $_ =~ m/()/){
+            push(@analisi, "[DATA] $_");
+        }
+    }
+}
+
+# Salvo gli ip in un file blacklist_data
+open(BLACKLIST,">","blacklist_data") or die $!;
+print BLACKLIST @analisi;
 close BLACKLIST;
